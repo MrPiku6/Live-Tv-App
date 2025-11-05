aa3433f4c58a974e605150c2c6e6821cf96fbc31
MrPiku6 <mrpiku323@gmail.com>
2025-11-05 13:28:35 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import ChannelList from '../components/ChannelList';
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ backgroundColor: '#000000', minHeight: '100vh' }}>
       <ChannelList />
     </main>
   );

c8de233d054191c1dbe7784edafd04aad5d0064c
MrPiku6 <mrpiku323@gmail.com>
2025-11-18 11:49:32 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import ChannelList from '../components/ChannelList';
-
-export default function Home() {
-  return (
-    <main style={{ backgroundColor: '#000000', minHeight: '100vh' }}>
-      <ChannelList />
-    </main>
-  );
-}
